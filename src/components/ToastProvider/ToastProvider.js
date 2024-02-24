847479593c2a7ed0bca225d423f79011fd203a17
brome40 <85357500+brome40@users.noreply.github.com>
2024-02-24 15:23:59 -0500

    Remove all Toasts on ESC

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const ToastContext = React.createContext();
 
@@ -18,6 +18,20 @@ function ToastProvider({ children }) {
       prevToastStack.filter(toast =>  toast.id !== id)
     ));
   }
+
+  useEffect(() => { // clear all toasts on Escape Key pressed
+    function handleEscKeyDown(event) {
+      if (event.code === 'Escape') {
+        setToastStack([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleEscKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscKeyDown);
+    };
+  },[]);
 
   return (
     <ToastContext.Provider
